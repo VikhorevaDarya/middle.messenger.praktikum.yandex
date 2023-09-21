e819143800bf4e44b9207e523e2ad16233003fc8
Darya <vikhorevadv@gmail.com>
2023-09-21 13:01:36 +0700

    test deploy

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,8 @@
 import './style.css'
 
-import viteLogo from '/vite.svg'
-
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="" class="logo vanilla" alt="TypeScript logo" />
