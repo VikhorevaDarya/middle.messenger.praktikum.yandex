9efdd2cf844e72f6891266a93c11af987d67c955
Darya <vikhorevadv@gmail.com>
2023-09-27 16:55:50 +0700

    add chat page

--- src/router/types.ts
+++
@@ -1,4 +0,0 @@
-export type RouteType = {
-  pathName: string, 
-  page: any
-}
--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,27 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 import pug from "vite-plugin-pug"
+import rollup from 'rollup-plugin-pug'
 
 export default defineConfig({
-  plugins: [pug()],
-  server: {
-    port: 3000
-  },
+  plugins: [pug({ localImports: true }, { baseUrl: './views' }), rollup()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
       'styles': resolve(__dirname, 'src/styles')
     }
+  },
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        'sign-in': resolve(__dirname, 'src/pages/SignIn/index.html'),
+        'sign-up': resolve(__dirname, 'src/pages/SignUp/index.html'),
+        'not-found': resolve(__dirname, 'src/pages/NotFound/index.html'),
+        'server-error': resolve(__dirname, 'src/pages/ServerError/index.html'),
+        'settings': resolve(__dirname, 'src/pages/Settings/index.html'),
+        'chat': resolve(__dirname, 'src/pages/Chat/index.html'),
+      }}
   }
 }) 
