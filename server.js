2e6305323748d17684245f61596e9e2e9bb20828
Darya <vikhorevadv@gmail.com>
2023-09-28 16:06:01 +0700

    fix server

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ app.use('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'))
 })
 
-app.use('/sign-in', express.static(path.join(__dirname, 'dist/src/pages/SignIn')));
-
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
