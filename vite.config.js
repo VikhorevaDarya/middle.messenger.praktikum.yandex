e1c855b99a42eaf31393dcb54ca49e3b33be1306
Darya <vikhorevadv@gmail.com>
2023-09-28 14:04:20 +0700

    fix imports

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   plugins: [
-    pug({ localImports: true }, { baseUrl: './views' }),
+    pug({ localImports: true }),
     rollup(),
     viteStaticCopy({
       targets: [
