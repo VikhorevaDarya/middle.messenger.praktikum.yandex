d00462ba1bdef5d4fee05443f55dbfb02f519701
Darya <vikhorevadv@gmail.com>
2023-10-07 14:54:49 +0700

    prettier format

--- src/utils/indentity.ts
+++ src/utils/indentity.ts
@@ -1,3 +1,3 @@
-export default function<T>(value: T): T {
-    return value
+export default function <T>(value: T): T {
+  return value
 }
--- src/utils/isEmpty.ts
+++ src/utils/isEmpty.ts
@@ -5,9 +5,8 @@ export default function isEmpty<T>(value: T): boolean | undefined {
     if (Array.isArray(value)) return value.length === 0
     if (isObject(value)) return Object.keys(value as Object).length === 0
     if (value instanceof Map || value instanceof Set) return value.size === 0
-    if (typeof value === 'string') return !Boolean(value) 
+    if (typeof value === 'string') return !Boolean(value)
   } else {
     return true
-  }   
+  }
 }
-
